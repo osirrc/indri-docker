e525d2d86e785ede68704609dcd57b7e8baccb01
Claudia Hauff <claudia@Claudias-MacBook-Air.local>
2019-06-11 01:56:11 +0200

    indexing semi-ok

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM ubuntu:18.04
+FROM ubuntu:14.04
 
-#install basics
+#install a few essentials
 RUN apt-get update && apt-get install -y \
     build-essential \
     wget \
@@ -9,12 +9,18 @@ RUN apt-get update && apt-get install -y \
     zlib1g-dev \
     python3
 
-COPY . /
-
 WORKDIR /work
-RUN chmod +x /init
-RUN chmod +x /index
-#COPY search /
 
-#RUN ["chmod", "+x", "/init"]
-#RUN ["chmod", "+x", "/index"]
+#download, configure and install Indri
+RUN wget --no-check-certificate https://sourceforge.net/projects/lemur/files/lemur/indri-5.11/indri-5.11.tar.gz
+RUN tar -xvzf indri-5.11.tar.gz -C /work
+RUN /work/indri-5.11/configure --prefix=/work/Indri
+RUN cd /work/indri-5.11 && cp ../Make* . && make && make install
+
+COPY index /index
+COPY index.sh /index.sh
+
+RUN chmod +x /index
+RUN chmod +x /index.sh
+
+#COPY search /
--- index.sh
+++ index.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-# Setup variables.
 COLLECTION_PATH=$1
 INDEX=$2
-#COLLECTION_FORMAT=$3
+COLLECTION_FORMAT=$3
 
 #parameter file for indexing
 touch index.param;
-echo "<parameters>\n" >> index.param;
-echo "<corpus><path>" >> index.param;
-echo ${COLLECTION_PATH} >> index.param;
-echo "</path></corpus>\n" >> index.param;
-echo "<class>trectext</class>\n" >> index.param;
-echo "</corpus>\n" >> index.param;
-echo "<index>" >> index.param;
-echo ${INDEX} >> index.param;
-echo "</index>\n" >> index.param;
+echo "<parameters>" >> index.param;
+echo "<index>/${INDEX}</index>" >> index.param;
+echo "<corpus>" >> index.param;
+echo "<path>${COLLECTION_PATH}</path>" >> index.param;
+echo "<class>${COLLECTION_FORMAT}</class>" >> index.param;
+echo "</corpus>" >> index.param;
 echo "</parameters>" >> index.param;
 
+more index.param
+/work/Indri/bin/IndriBuildIndex index.param
 
-`/work/Indri/bin/IndriBuildIndex index.param`
+#ls -al /robust/index/1/
+#more /robust/index/1/manifest
