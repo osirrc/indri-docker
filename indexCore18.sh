4be1d20d404d4f6942789e7c10aca1a64516a128
Claudia <claudia@wlan-145-94-218-130.wlan.tudelft.nl>
2019-06-21 16:13:24 +0200

    core18 parser absolute path

--- indexCore18.sh
+++ indexCore18.sh
@@ -6,7 +6,7 @@ COLLECTION_FORMAT=$3
 COLLECTION_PATH_WRITABLE=$1"-WRITABLE"
 
 #first we need to parse the collection
-nodejs parseCore18.js ${COLLECTION_PATH}/data/* ${COLLECTION_PATH_WRITABLE}
+nodejs /parseCore18.js ${COLLECTION_PATH}/data/* ${COLLECTION_PATH_WRITABLE}
 
 #retrieve stopword list (stored in current directory)
 wget http://www.lemurproject.org/stopwords/stoplist.dft
